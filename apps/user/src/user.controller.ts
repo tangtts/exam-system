7516c1a2d6d2aa6d0fd552a91512f4710554ba97
TSK2 <2939117014@qqcom>
2024-10-21 16:54:28 +0800

    添加校验功能

--- apps/user/src/user.controller.ts
+++ apps/user/src/user.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RedisService } from '@app/redis';
+import { RegisterUserDto } from './dto/register-user.dto';
 
-@Controller()
+@Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Inject(RedisService)
   redisService: RedisService;
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    delete registerUser.captcha;
+    return await this.userService.create(registerUser);
+  }
 
   @Get()
   async getHello() {
     const keys = await this.redisService.keys('*');
-    return this.userService.getHello() +  keys;
+    return this.userService.getHello() + keys;
   }
 }
--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
+
+
+  @Inject(PrismaService)
+  private prisma: PrismaService;
+
   getHello(): string {
     return 'Hello World!';
   }
+
+
+  async create(data: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data,
+      select: {
+        id: true
+      }
+    });
+  }
 }
+
